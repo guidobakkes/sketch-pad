26113c848f04cfc5f6850d6601581af71dbcc156
Guido Bakkes <gbakkes@gmail.com>
2016-06-22 16:43:20 +0200

    only ask for total size instead of height+width, remove individual borders, add border to container

--- sketchPad.js
+++ sketchPad.js
@@ -10,17 +10,16 @@ function init () {
         $('body').append('<div id="container"</div>');
 
         // set the height and width of the canvas
-        var height = prompt("Enter height: ");
-        var width = prompt("Enter width: ");
+        var size = prompt("What size grid would you like?");
 
         // container height with ul's
-        for (var i = 0; i < height; i++) {
+        for (var i = 0; i < size; i++) {
             $('#container').append("<ul></ul>");
         }
 
         // fill ul with li for width
-        if (width < 31) {
-            for (var j = 0; j < width; j++) {
+        if (size < 31) {
+            for (var j = 0; j < size; j++) {
                 $('ul').append('<li class="pixel"></li>');
             }
         } else {
